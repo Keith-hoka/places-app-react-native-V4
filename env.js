abe60afcc12993eef4a31ff68d8f8b385e8e27fd
Keith-hoka <75162022+Keith-hoka@users.noreply.github.com>
2021-09-06 00:33:52 +1000

    Add files via upload

---
+++ env.js
@@ -0,0 +1,23 @@
+const vars = {
+  googleApiKey: `AIzaSyDA2BL7w_xl3ItKErPS5AjwnEsD69iJrhw`,
+};
+
+export default vars;
+
+// const variables = {
+//     development: {
+//         googleApiKey: 'abc'
+//     },
+//     production: {
+//         googleApiKey: 'xyz'
+//     }
+// };
+ 
+// const getEnvVariables = () => {
+//     if (__DEV__) {
+//         return variables.development; // return this if in development mode
+//     }
+//     return variables.production; // otherwise, return this
+// };
+ 
+// export default getEnvVariables; // export a reference to the function
